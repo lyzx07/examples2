b3172e3f1dc75533a0d4ef3ab665eae5813c85d4
lyzx07 <86989628+lyzx07@users.noreply.github.com>
2023-03-27 18:03:39 -0700

    form design

--- idea/app.py
+++ idea/app.py
@@ -6,12 +6,33 @@ import random
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure SQLite database
+conn = sqlite3.connect('rate-app.db', check_same_thread=False)
+c = conn.cursor()
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
--- idea/static/script.js
+++ idea/static/script.js
@@ -5,7 +5,7 @@ let pic2 = document.getElementById('pic2');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 + 'px';
+    text.style.marginBottom = value * 2.5 + 'px';
     pic1.style.bottom = value * 1.5 + 'px';
     pic1.style.right = value * -1.5 + 'px';
     pic2.style.left = value * -1.5 + 'px';
