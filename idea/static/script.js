9a49856a43ae122c3895e7abfc5879799c5f6744
lyzx07 <86989628+lyzx07@users.noreply.github.com>
2023-04-02 23:21:46 -0700

    adding modal registration form

--- idea/static/script.js
+++ idea/static/script.js
@@ -11,3 +11,29 @@ window.addEventListener('scroll', () => {
     pic2.style.left = value * -1.5 + 'px';
     pic2.style.bottom = value * 1.5 + 'px';
 })
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
