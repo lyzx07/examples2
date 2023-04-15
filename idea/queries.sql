2c6f5a31977efce4d3cbdb20cb16f61ba3a44244
lyzx07 <86989628+lyzx07@users.noreply.github.com>
2023-04-14 22:32:02 -0700

    working with variables and dealing with db journal

--- idea/queries.sql
+++ idea/queries.sql
@@ -8,3 +8,9 @@ SELECT hash FROM users;
 
 
 SELECT hash FROM users WHERE username = 'lyzx'
+
+ATTACH 'rate-app.db-journal' AS journal;
+PRAGMA journal_mode = DELETE;
+BEGIN TRANSACTION;
+INSERT INTO main.rate-app SELECT * FROM journal.rate-app;
+COMMIT;
