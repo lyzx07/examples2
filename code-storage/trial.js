8eb1b5598961d0f406e91b3f3d6f8218ba197192
lyzx07 <86989628+lyzx07@users.noreply.github.com>
2023-06-07 21:43:55 -0700

    first commit code-stotage

---
+++ code-storage/trial.js
@@ -0,0 +1,19 @@
+// Prompt user for Website information
+let purpose = prompt("What is the purpose of the website?");
+let scope = prompt("What is the scope of the website?");
+let content = prompt("What type of content will be on the website?");
+let design = prompt("What are your design preferences?");
+let branding = prompt("Do you have any branding guidelines to follow?");
+let budget = prompt("What's your budget for the website?");
+let timeline = prompt("What is your timeline for building the website?");
+
+// Log user inputs to console
+console.log("Purpose of website:", purpose);
+console.log("Scope of website:", scope);
+console.log("Type of content:", content);
+console.log("Design preferences:", design);
+console.log("Branding guidelines:", branding);
+console.log("Website budget:", budget);
+console.log("Website timeline:", timeline);
+
+
