aa75027371a3c9c305a58352ad64fcbae94f2bb0
lyzx07 <86989628+lyzx07@users.noreply.github.com>
2023-06-26 23:01:01 -0700

    styling changes phrases

--- idea/static/pentatonix.js
+++ idea/static/pentatonix.js
@@ -1,29 +1,29 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  
-  $(document).ready(function() {
-    var showMore = true; // Set initial state, true = show more
-  
-    $('.btn-show-more').on('click', function() {
-      var $btn = $(this);
-      var $list = $btn.prev('.song-list');
-      
-      if (showMore) {
-        $list.css('max-height', 'none');
-        $btn.text('Show Less');
-      } else {
-        $list.css('max-height', '');
-        $btn.text('Show More');
-      }
-  
-      showMore = !showMore; // Toggle state
-    });
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+$(document).ready(function () {
+  var showMore = true; // Set initial state, true = show more
+
+  $(".btn-show-more").on("click", function () {
+    var $btn = $(this);
+    var $list = $btn.prev(".song-list");
+
+    if (showMore) {
+      $list.css("max-height", "none");
+      $btn.text("Show Less");
+    } else {
+      $list.css("max-height", "");
+      $btn.text("Show More");
+    }
+
+    showMore = !showMore; // Toggle state
   });
-  
+});
+
